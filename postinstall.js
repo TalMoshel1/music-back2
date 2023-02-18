441842d542da89837d1e2bd4545889b4ff074932
TalMoshel1 <89250740+TalMoshel1@users.noreply.github.com>
2023-02-18 17:00:53 +0200

    add files

---
+++ postinstall.js
@@ -0,0 +1,14 @@
+const { execSync } = require('child_process');
+
+function runBuildScript() {
+  console.log('Running build script...');
+  try {
+    execSync('npm run build', { stdio: 'inherit' });
+    console.log('Build script completed successfully.');
+  } catch (error) {
+    console.error('Error running build script:', error);
+    process.exit(1);
+  }
+}
+
+runBuildScript();
