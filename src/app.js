448622c26dd7d3275145cd3f9691817efa7598ca
unknown <talmoshel444@gmail.com>
2024-05-20 15:32:22 +0300

    pushBackend3

--- src/app.js
+++ src/app.js
@@ -1,20 +1,18 @@
-import express from 'express';
-import router from './routes/index.js'
-import connectToDb from './db/connectToDb.js'
-import cors from 'cors'
+import express from "express";
+import router from "./routes/index.js";
+import connectToDb from "./db/connectToDb.js";
+import cors from "cors";
 
-const FE_RENDER = 'https://music-trivia-frontend.onrender.com' 
+const FE_RENDER = "https://music-trivia-1.onrender.com//";
 const app = express();
-app.use(cors({ origin: process.env.FE_URL|| FE_RENDER , credentials: true }))
+app.use(cors({ origin: process.env.FE_URL || FE_RENDER, credentials: true }));
 
-
-app.use(express.json())
-app.use(router)
+app.use(express.json());
+app.use(router);
 
 const PORT = process.env.PORT || 3000;
 
-connectToDb()
-.then(()=>{
-    app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
-    console.log('cors wating for requests from: ',FE_RENDER)
-})
+connectToDb().then(() => {
+  app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
+  console.log("cors wating for requests from: ", FE_RENDER);
+});
